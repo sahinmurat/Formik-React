f3fecb262d4f3bdc9c1d58c388b7c07479a7fefc
Murat Sahin <yazilimogren1111@gmail.com>
2020-12-03 14:33:25 +0100

    Yup added for validation

--- src/components/Selbst.js
+++ src/components/Selbst.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { useFormik } from 'formik';
-
-const validate = (values) => {
-    let errors = {};
-    if (!values.name) errors.name = 'Requiered Field!';
-    if (!values.email) { errors.email = 'Requiered Field'; }
-    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = "Invalid email format";
-    }
-    if (!values.company) errors.company = 'Requiered Field!';
-    return errors;
-}
+import * as Yup from 'yup';
+const validate = Yup.object({
+    name:Yup.string().required('Requiered Field'),
+    email:Yup.string().email('invalid email format').required('Requiered Field'),
+    company:Yup.string().max(15, 'verylong').required('Requiered Field')
+})
 
 const initialValues = {
     name: '',
@@ -24,40 +19,37 @@ const onSubmit = (values) => {
 
 const Selbst = () => {
     const formik = useFormik({
-        validate,
+        validationSchema:validate,
         onSubmit,
         initialValues
     })
     return (
         <form onSubmit={formik.handleSubmit}>
-            <div>
+            <div className='form-control'>
                 <label htmlFor='name' >Name</label>
                 <input type='text' id='name' name='name' 
-                // value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur}
                 {...formik.getFieldProps('name')}
                  />
                 {formik.touched.name && formik.errors.name ? (
                     <div className='error'> {formik.errors.name} </div>) : null}
             </div>
-            <div>
+            <div  className='form-control'>
                 <label htmlFor='email'>Email</label>
                 <input type='email' id='email' name='email' 
-                // value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}
                 {...formik.getFieldProps('email')}
                  />
                 {formik.touched.email && formik.errors.email ? (
                     <div className='error'> {formik.errors.email} </div>) : null}
             </div>
-            <div>
+            <div className='form-control'>
                 <label htmlFor='company'>Company</label>
                 <input type='text' id='company' name='company'
-                //  value={formik.values.company} onChange={formik.handleChange} onBlur={formik.handleBlur} 
                 {...formik.getFieldProps('company')}
                 />
                 {formik.touched.company && formik.errors.company ? (
                     <div className='error'> {formik.errors.company} </div>) : null}
             </div>
-            <div>
+            <div className='form-control'>
                 <button type='submit'>Submit</button>
             </div>
         </form>
