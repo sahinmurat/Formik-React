19cbb27891c9efd287477d43a852acee44c44036
Murat Sahin <yazilimogren1111@gmail.com>
2020-12-03 14:47:37 +0100

    formiks third and easiest form added

--- src/components/Selbst.js
+++ src/components/Selbst.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 const validate = Yup.object({
     name:Yup.string().required('Requiered Field'),
@@ -8,7 +8,7 @@ const validate = Yup.object({
 })
 
 const initialValues = {
-    name: '',
+    name: 'What is your name?',
     email: '',
     company: ''
 }
@@ -18,41 +18,37 @@ const onSubmit = (values) => {
 }
 
 const Selbst = () => {
-    const formik = useFormik({
-        validationSchema:validate,
-        onSubmit,
-        initialValues
-    })
     return (
-        <form onSubmit={formik.handleSubmit}>
+        <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validate}
+        >
+        <Form >
             <div className='form-control'>
                 <label htmlFor='name' >Name</label>
-                <input type='text' id='name' name='name' 
-                {...formik.getFieldProps('name')}
-                 />
-                {formik.touched.name && formik.errors.name ? (
-                    <div className='error'> {formik.errors.name} </div>) : null}
+                <Field type='text' id='name' name='name'/>
+                <ErrorMessage name='name' component='div'/>
             </div>
             <div  className='form-control'>
                 <label htmlFor='email'>Email</label>
-                <input type='email' id='email' name='email' 
-                {...formik.getFieldProps('email')}
+                <Field type='email' id='email' name='email' 
                  />
-                {formik.touched.email && formik.errors.email ? (
-                    <div className='error'> {formik.errors.email} </div>) : null}
+                  <ErrorMessage name='email' component='div'/>
             </div>
             <div className='form-control'>
                 <label htmlFor='company'>Company</label>
-                <input type='text' id='company' name='company'
-                {...formik.getFieldProps('company')}
+                <Field type='text' id='company' name='company'
                 />
-                {formik.touched.company && formik.errors.company ? (
-                    <div className='error'> {formik.errors.company} </div>) : null}
+                 <ErrorMessage name='company' component='div'/>
+
+             
             </div>
             <div className='form-control'>
                 <button type='submit'>Submit</button>
             </div>
-        </form>
+        </Form>
+        </Formik>
     )
 }
 export default Selbst;
