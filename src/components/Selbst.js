48c92d29370e54bf3880018103edba97f9f67b01
Murat Sahin <yazilimogren1111@gmail.com>
2020-12-03 14:24:57 +0100

    formiks second form added

--- src/components/Selbst.js
+++ src/components/Selbst.js
@@ -32,19 +32,28 @@ const Selbst = () => {
         <form onSubmit={formik.handleSubmit}>
             <div>
                 <label htmlFor='name' >Name</label>
-                <input type='text' id='name' name='name' value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                <input type='text' id='name' name='name' 
+                // value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur}
+                {...formik.getFieldProps('name')}
+                 />
                 {formik.touched.name && formik.errors.name ? (
                     <div className='error'> {formik.errors.name} </div>) : null}
             </div>
             <div>
                 <label htmlFor='email'>Email</label>
-                <input type='email' id='email' name='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                <input type='email' id='email' name='email' 
+                // value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}
+                {...formik.getFieldProps('email')}
+                 />
                 {formik.touched.email && formik.errors.email ? (
                     <div className='error'> {formik.errors.email} </div>) : null}
             </div>
             <div>
                 <label htmlFor='company'>Company</label>
-                <input type='text' id='company' name='company' value={formik.values.company} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                <input type='text' id='company' name='company'
+                //  value={formik.values.company} onChange={formik.handleChange} onBlur={formik.handleBlur} 
+                {...formik.getFieldProps('company')}
+                />
                 {formik.touched.company && formik.errors.company ? (
                     <div className='error'> {formik.errors.company} </div>) : null}
             </div>
